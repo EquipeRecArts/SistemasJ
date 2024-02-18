44f53bc70d4208fc0af15cd464c90e8823968ad3
thidspessoa <thiagopessoa246@gmail.com>
2024-02-18 20:48:48 -0300

    Todas as labels necessarias paginadas na versao desktop

--- assets/tableScriptBootstrap.js
+++ assets/tableScriptBootstrap.js
@@ -1,3 +1,14 @@
-new DataTable('#example');
-new DataTable('#example-2');
-new DataTable('#example-3');
+const id = '#example';
+const totalElements = 24;
+
+
+for (let i = 0; i <= totalElements; i++) {
+    let parameter = `${id}-${i}`
+    new DataTable(parameter);
+}
+
+
+// new DataTable('#example');
+// new DataTable('#example-2');
+// new DataTable('#example-3');
+// new DataTable('#example-4');
