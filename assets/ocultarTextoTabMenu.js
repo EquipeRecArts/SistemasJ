bba56c42c6124dd476414391e107fc1735e6d60d
thidspessoa <thiagopessoa246@gmail.com>
2024-02-19 18:51:11 -0300

    Finalizado ajustes na responsividade tablet e mobile

--- assets/ocultarTextoTabMenu.js
+++ assets/ocultarTextoTabMenu.js
@@ -3,19 +3,17 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener('resize', function(){
-        const larguraDaTela = window.innerWidth;
-        const listaElementos = ['text_container-hot_phone', 'text_container-hot_mobile_phone', 'text_container-hot_address', 'text_container-hot_email'];
-        if (larguraDaTela <= 937) {
-            for (let classe of listaElementos) {
-                let elementoDom = document.querySelector("." + classe);
-                elementoDom.classList.add('disabled');
-            }
-        } else if (larguraDaTela > 937) {
-            for (let classe of listaElementos) {
-                let elementoDom = document.querySelector("." + classe);
-                elementoDom.classList.remove('disabled')
-            }
+    const larguraDaTela = window.innerWidth;
+    const listaElementos = ['text_container-hot_phone', 'text_container-hot_mobile_phone', 'text_container-hot_address', 'text_container-hot_email'];
+    if (larguraDaTela <= 937) {
+        for (let classe of listaElementos) {
+            let elementoDom = document.querySelector("." + classe);
+            elementoDom.classList.add('disabled');
         }
-    })
+    } else if (larguraDaTela > 937) {
+        for (let classe of listaElementos) {
+            let elementoDom = document.querySelector("." + classe);
+            elementoDom.classList.remove('disabled')
+        }
+    }
 })
